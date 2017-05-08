6d71ae5234622a7184deb07b1a613ea10e8b4db9
Ryan Masilionis <ryan.masilionis@gmail.com>
2017-05-07 19:03:36 -0500

    Added "AutoMapper" package

--- AssetManagement-WebApp/Controllers/Api/AssetController.cs
+++ AssetManagement-WebApp/Controllers/Api/AssetController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AssetManagementWebApp.Models;
 
 namespace AssetManagement_WebApp.Controllers
 {
+    [Route("/api/assets")]
     public class AssetController : Controller
     {
-        //[Route("Assets")]
-        [HttpGet("Assets")]
-        public IActionResult Index()
+        private IAssetRepository _repository;
+        public AssetController(IAssetRepository repository)
         {
+            _repository = repository;
+        }
+        
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            try
+            {
+                var assets = _repository.GetAllAssets();
+                //return Ok(Mapper.)
+            }
             return Json("Test");
             //return View();
         }
