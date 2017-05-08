b55140ca5d243807b8f3005184bc0405beb25f01
Ryan Masilionis <ryan.masilionis@gmail.com>
2017-05-07 23:31:53 -0500

    Development of the Application controller

--- AssetManagement-WebApp/Controllers/Web/ApplicationController.cs
+++ AssetManagement-WebApp/Controllers/Web/ApplicationController.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AssetManagementWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AssetManagement_WebApp.Controllers
 {
     public class ApplicationController : Controller
     {
+        private IAssetRepository _repository;
+        public ApplicationController(IAssetRepository repository)
+        {
+            _repository = repository;
+        }
+
         [Route("Applications")]
         public IActionResult Index()
         {
-            ViewData["Message"] = "Your application description page.";
             return View();
+        }
+
+        public IActionResult Applications()
+        {
+            var applications = _repository.GetAllAssets().Where(t => t.AssetType.Id == AssetTypeEnum.Application);
+
+            return View(applications);
         }
     }
 }
