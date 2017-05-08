fd0976c215dfcaad8dcacc43cc4205601af5abbf
Ryan Masilionis <ryan.masilionis@gmail.com>
2017-05-08 14:29:13 -0500

    Pulling Asset data from database via Angular and Assets controller

--- AssetManagement-WebApp/wwwroot/js/assetsController.js
+++ AssetManagement-WebApp/wwwroot/js/assetsController.js
@@ -7,22 +7,55 @@
     angular.module("app-assets")
         .controller("assetsController", assetsController);
 
-    function assetsController() {
+    function assetsController($http) {
         var vm = this;
 
-        vm.assets = [{
-            name: "AssetName",
-            created: new Date()
-        }, {
-            name: "Asset2Name",
-            created: new Date()
-        }];
+        //vm.assets = [{
+        //    name: "AssetName",
+        //    created: new Date()
+        //}, {
+        //    name: "Asset2Name",
+        //    created: new Date()
+        //}];
+
+        vm.assets = [];
 
         vm.newAsset = {};
 
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+        $http.get("/api/assets")
+            .then(function (response) {
+                // Success
+                angular.copy(response.data, vm.assets);
+            }, function (error) {
+                // Failure
+                vm.errorMessage = "Failed to load data: " + error;
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            });
+
         vm.addAsset = function () {
-            vm.assets.push({ name: vm.newAsset.name, created: new Date() });
-            vm.newAsset = {};
+            //vm.assets.push({ name: vm.newAsset.name, created: new Date() });
+            //vm.newAsset = {};
+
+            vm.isBusy = true;
+            vm.errorMessage = "";
+
+            $http.post("/api/assets", vm.newAsset)
+                .then(function (response) {
+                    // success
+                    vm.assets.push(response.data)
+                    vm.newAsset = {};
+                }, function () {
+                    // failure
+                    vm.errorMessage = "Failed to save new asset"
+                })
+                .finally(function () {
+                    vm.isBusy = false;
+                });
         };
 
 
